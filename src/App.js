f0f77b096653249f0a7608586885e62b2946655c
kishore230722 <148748576+kishore230722@users.noreply.github.com>
2023-11-02 17:20:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from './products';
+import { useState,useEffect } from 'react'
+import Sidebar from './Sidebar'
+import products from './data'
+import Items from './items';
+
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+const [totalPages, setTotalPages] = useState(0);
+const itemsPerPage = 12
+
+
+
+const handlePageChange = (selectedPage) => {
+  setCurrentPage(selectedPage.selected);
+};
+ 
+
+ 
+ 
+
+ 
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  
+ 
+  
+ 
+  
+  useEffect (() =>{
+    
+    setTotalPages(Math.ceil(products.length/itemsPerPage))
+    
+   },[])
+
+ 
+  
+
+  const FilteredData = (products, selected)=> {
+    let filteredProducts = products;
+
+   
+   
+
+    
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, priceRange }) =>
+          category === selected ||
+          priceRange === selected
+      );
+      
+     
+    }
+    let startIndex = currentPage * itemsPerPage
+      let endIndex = startIndex + itemsPerPage
+      const subset = filteredProducts.slice(startIndex,endIndex);
+     
+         
+        
+         
+          
+       
+        
+      
+     
+      
+ 
+
+    return subset.map(
+      ({ img, title, description, price }) => (
+        <Items
+          
+          img={img}
+          title={title}
+          
+          
+          price={price}
+          description={description}
+        />
+      )
+    );
+  }
+
+  const result = FilteredData(products, selectedCategory);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Sidebar handleChange={handleChange} />
+    <Products result = {result} totalPages={totalPages} currentPage={currentPage} handlePageChange={handlePageChange}/>
+    
+
+    </>
+  )
 }
 
 export default App;
+
